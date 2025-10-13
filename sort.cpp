238a1af69e0bf74bda6fe0e2fadfd336225959cf
Yulia <yu45753@yandex.ru>
2025-10-14 00:00:11 +0300

    добавлено заполнение случайными числами

--- sort.cpp
+++ sort.cpp
@@ -38,7 +38,7 @@ void keyboard_input(vector<int>& arr){
 
     int size = 0;
     do {
-        cout << "Введите размер массива: " << endl;
+        cout << "Введите размер массива: ";
         get_int(size);
 
         if (size < 1)
@@ -55,8 +55,60 @@ void keyboard_input(vector<int>& arr){
 }
 
 // заполнение рандомными числами
-void random_input(){
-    
+enum range_input {without_range = 1, with_range};
+
+void random_input(vector<int>& arr){
+    int size = 0;
+    do {
+        cout << "Введите размер массива: ";
+        get_int(size);
+
+        if (size < 1)
+            cout << "Некорректный размер массива.";
+    } while(size < 1);
+
+    int user_choice = 0;
+    do {
+        cout << "Выберите способ случайного заполнения: " << endl
+             << "1 - Заполнение случайными числами без диапозона;" << endl
+             << "2 - Заполнение случайными числами в выбранном диапозоне." << endl;
+
+    cout << "Введите число: ";
+    get_int(user_choice);
+
+    if (user_choice != 1 && user_choice != 2)
+        cout << "Некорректный пункт меню." << endl;
+
+    } while (user_choice != 1 && user_choice != 2);
+
+    switch (user_choice)
+    {
+    case without_range:
+        for (size_t i = 0; i < size; i++)
+            arr.push_back(rand());
+    break;
+
+    case with_range:
+        
+        int first_range_num, second_range_num;
+        do {
+            cout << "Введите первое число диапазона: ";
+            get_int(first_range_num);
+
+            cout << "Введите второе число диапазона: ";
+            get_int(second_range_num);
+
+            if (first_range_num > second_range_num)
+                cout << "Некорректный диапазон." << endl;
+        } while(first_range_num > second_range_num);
+
+        int range_size = second_range_num - first_range_num + 1;
+
+        for (size_t i = 0; i < size; i++)
+            arr.push_back(rand() % range_size + first_range_num);
+
+    break;
+    }
 }
 
 // ввод из файла
@@ -66,16 +118,18 @@ void file_input(){
 
 // вывод массива
 void print_array(vector<int>& array_to_print){
+    cout << endl;
     for (int num : array_to_print)
         cout << num << " ";
-    cout << endl;
+    cout << endl << endl;
 }
 
 // главное меню
 enum menu_items {keyboard = 1, random_items, file, prog_exit};
 
 int main(){
-
+    srand(0);
+    
     vector<int> array_to_sort;
     int user_choice = 0;
    
@@ -96,14 +150,23 @@ int main(){
         {
         case keyboard:
             keyboard_input(array_to_sort);
-            break;
+            print_array(array_to_sort);
+            array_to_sort.clear();
+            user_choice = 0;
+        break;
 
         case random_items:
-            random_input();
-            break;
+            random_input(array_to_sort);
+            print_array(array_to_sort);
+            array_to_sort.clear();
+            user_choice = 0;
+        break;
 
         case file:
             file_input();
+            print_array(array_to_sort);
+            array_to_sort.clear();
+            user_choice = 0;
         break;  
 
         case prog_exit:
@@ -111,9 +174,8 @@ int main(){
         
         default:
             cout << "Некорректный пункт меню." << endl;
-            break;
+        break;
         }
 
     } while(user_choice != prog_exit);
-
 }
